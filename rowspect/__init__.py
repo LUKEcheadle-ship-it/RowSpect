9d843451faae9950093413670ee9240c474fb401
Luke Cheadle <LUKEcheadle@icloud.com>
2026-09-08 12:09:37 -0500

    feat: expose V1 public API

--- rowspect/__init__.py
+++ rowspect/__init__.py
@@ -1,8 +1,22 @@
-"""RowSpect: local-first data quality profiling for tabular data."""
+"""RowSpect public API."""
 
-from .clean import clean_dataframe
-from .profile import profile_dataframe
-from .report import build_html_report
+from rowspect.clean import clean_dataframe, cleanup_summary
+from rowspect.export import dataframe_to_csv, dataframe_to_xlsx, export_safe_dataframe
+from rowspect.io import RowSpectIOError, get_excel_sheets, load_table
+from rowspect.profile import profile_dataframe
+from rowspect.report import build_html_report
 
-__all__ = ["profile_dataframe", "clean_dataframe", "build_html_report"]
-__version__ = "0.1.0"
+__all__ = [
+    "RowSpectIOError",
+    "build_html_report",
+    "clean_dataframe",
+    "cleanup_summary",
+    "dataframe_to_csv",
+    "dataframe_to_xlsx",
+    "export_safe_dataframe",
+    "get_excel_sheets",
+    "load_table",
+    "profile_dataframe",
+]
+
+__version__ = "1.0.0"
